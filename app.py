cbbdff9857e4b1e078b6683e3a7612e39d4b18ee
Breno Queiroz <brenoqueiroz@MacBook-Air-de-Breno.local>
2024-10-09 12:31:21 -0300

    adicionado filtro de data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import dash
 from dash import dcc, html
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 from wordcloud import WordCloud
@@ -73,6 +74,7 @@ app.layout = html.Div(
                     style={"width": "50px", "gap": "10px"},
                 ),
                 html.H1("Dashboard"),
+                
             ],
             style={
                 "background": "#08306b",
@@ -81,77 +83,20 @@ app.layout = html.Div(
                 "display": "flex",
             },
         ),
+        
         html.Div(
             [
-                html.Div(
-                    [
-                        html.Div(
-                            ("Tempo Médio de Conversa:"),
-                            style={
-                                "margin-top": "10px",
-                                "margin-left": "20px",
-                                "margin-right": "20px",
-                                "margin-bottom": "10px",
-                            },
-                        ),
-                        html.Div(
-                            f"{media_tempo_conversa:.2f} min",
-                            style={"font-weight": "bold"},
-                        ),
-                    ],
-                    style={
-                        "height": "100px",
-                        "width": "14%",
-                        "background-color": "#fefefc",
-                        "border-radius": "20px",
-                        "box-shadow": "0 4px 15px rgba(0, 0, 0, 0.2)",
-                        "transform": "translateY(-5px)",
-                        "display": "flex",
-                        "align-items": "center",
-                        "justify-content": "center",
-                        "flex-direction": "column",
-                    },
-                ),
-                html.Div(
-                    [
-                        html.Div(
-                            (f"Média de Mensagens por Conversa:"),
-                            style={
-                                "margin-top": "10px",
-                                "margin-left": "10px",
-                                "margin-right": "10px",
-                                "margin-bottom": "10px",
-                            },
-                        ),
-                        html.Div(
-                            (f"{media_mensagens_por_conversa:.2f}"),
-                            style={"font-weight": "bold"},
-                        ),
-                    ],
-                    style={
-                        "height": "100px",
-                        "width": "14%",
-                        "background-color": "#fefefc",
-                        "border-radius": "20px",
-                        "box-shadow": "0 4px 15px rgba(0, 0, 0, 0.2)",
-                        "transform": "translateY(-5px)",
-                        "border-radius": "20px",
-                        "display": "flex",
-                        "align-items": "center",
-                        "justify-content": "center",
-                        "flex-direction": "column",
-                    },
+                dcc.DatePickerRange(
+                    id="date-picker",
+                    min_date_allowed=df["data"].min(),
+                    max_date_allowed=df["data"].max(),
+                    start_date=df["data"].min(),
+                    end_date=df["data"].max(),
                 ),
             ],
-            style={
-                "margin-left": "25px",
-                "margin-top": "30px",
-                "display": "flex",
-                "align-items": "center",
-                "flex-direction": "row",
-                "gap": "20px",
-            },
+            style={"margin": "20px"},
         ),
+    
         html.Div(
             [
                 html.Div(
@@ -275,6 +220,84 @@ app.layout = html.Div(
                 ),
                 html.Div(
                     [
+                          html.Div(
+            [
+                html.Div(
+                    [
+                        html.Div(
+                            ("Tempo Médio de Conversa:"),
+                            style={
+                                "margin-top": "10px",
+                                "margin-left": "10px",
+                                "margin-right": "10px",
+                                "margin-bottom": "10px",
+                                "display": "flex",
+                                "align-items": "center",
+                                "justify-content": "center",
+                                
+                                
+                            },
+                        ),
+                        html.Div(
+                            f"{media_tempo_conversa:.2f} min",
+                            style={"font-weight": "bold", "font-size":"30px"},
+                        ),
+                    ],
+                    style={
+                        "height": "192px",
+                        "width": "90%",
+                        "background-color": "#fefefc",
+                        "border-radius": "20px",
+                        "box-shadow": "0 4px 15px rgba(0, 0, 0, 0.2)",
+                        "transform": "translateY(-5px)",
+                        "display": "flex",
+                        "align-items": "center",
+                        "justify-content": "center",
+                        "flex-direction": "column",
+                    },
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            (f"Média de Mensagens por Conversa:"),
+                            style={
+                                "margin-top": "10px",
+                                "margin-left": "20px",
+                                "margin-right": "10px",
+                                "margin-bottom": "10px",
+                            },
+                        ),
+                        html.Div(
+                            (f"{media_mensagens_por_conversa:.2f}"),
+                            style={"font-weight": "bold", "font-size":"30px"},
+                        ),
+                    ],
+                    style={
+                        "height": "192px",
+                        "width": "90%",
+                        "background-color": "#fefefc",
+                        "border-radius": "20px",
+                        "box-shadow": "0 4px 15px rgba(0, 0, 0, 0.2)",
+                        "transform": "translateY(-5px)",
+                        "border-radius": "20px",
+                        "display": "flex",
+                        "align-items": "center",
+                        "justify-content": "center",
+                        "flex-direction": "column",
+                    },
+                ),
+            ],
+            style={
+            
+                "margin-top": "5px",
+                "display": "flex",
+                "align-items": "center",
+                "flex-direction": "column",
+                "gap": "20px",
+                "width":"100%"
+            },
+        ),
+                        
                         dcc.Graph(
                             id="total-diario",
                             figure=px.bar(
@@ -437,5 +460,128 @@ app.layout = html.Div(
     ]
 )
 
+@app.callback(
+    [
+        Output("respostas-por-dia", "figure"),
+        Output("mapa-origem", "figure"),
+        Output("media-conversas-por-dia", "figure"),
+        Output("total-diario", "figure"),
+        Output("tempo-medio-chat", "figure"),
+        Output("total-interacoes", "figure"),
+        Output("avaliacao-conversa", "figure"),
+        Output("feedback", "figure"),
+    ],
+    [Input("date-picker", "start_date"), Input("date-picker", "end_date")]
+)
+def update_graphs(start_date, end_date):
+    filtered_df = df[(df["data"] >= start_date) & (df["data"] <= end_date)]
+    fig_respostas = px.line(
+        filtered_df,
+        x="data",
+        y="respostas",
+        title="Número de Respostas por Dia",
+        color_discrete_sequence=["#636EFA"]
+    )
+    fig_respostas.update_layout(
+        plot_bgcolor="rgba(255, 255, 255, 0.9)",
+        paper_bgcolor="rgba(255, 255, 255, 0.9)",
+    )
+
+    fig_mapa = px.choropleth(
+        filtered_df,
+        geojson="https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/brazil-states.geojson",
+        locations="local",
+        featureidkey="properties.name",
+        color="total_interacoes",
+        hover_name="local",
+        title="Origem das Conversas por Estado (Brasil)",
+        color_continuous_scale="Blues",
+        scope="south america",
+    )
+    fig_mapa.update_layout(
+        plot_bgcolor="rgba(255, 255, 255, 0.9)",
+        paper_bgcolor="rgba(255, 255, 255, 0.9)",
+    )
+
+    fig_media_conversas = px.line(
+        filtered_df,
+        x="data",
+        y="media_conversas_por_dia",
+        title="Número Médio de Conversas por Dia",
+    )
+    fig_media_conversas.update_layout(
+        plot_bgcolor="rgba(255, 255, 255, 0.9)",
+        paper_bgcolor="rgba(255, 255, 255, 0.9)",
+    )
+
+    fig_total_diario = px.bar(
+        filtered_df,
+        x="data",
+        y="respostas",
+        title="Total de Conversas por Dia",
+    )
+    fig_total_diario.update_layout(
+        plot_bgcolor="rgba(255, 255, 255, 0.9)",
+        paper_bgcolor="rgba(255, 255, 255, 0.9)",
+    )
+
+
+    fig_tempo_medio_chat = px.line(
+        filtered_df,
+        x="data",
+        y="tempo_medio_chat",
+        title="Tempo Médio de um Chat",
+    )
+    fig_tempo_medio_chat.update_layout(
+        plot_bgcolor="rgba(255, 255, 255, 0.9)",
+        paper_bgcolor="rgba(255, 255, 255, 0.9)",
+    )
+
+    fig_total_interacoes = px.bar(
+        filtered_df,
+        x="data",
+        y="total_interacoes",
+        title="Total de Interações",
+    )
+    fig_total_interacoes.update_layout(
+        plot_bgcolor="rgba(255, 255, 255, 0.9)",
+        paper_bgcolor="rgba(255, 255, 255, 0.9)",
+    )
+
+    fig_avaliacao_conversa = px.bar(
+        filtered_df,
+        x="data",
+        y="avaliacao_conversa",
+        title="Avaliação da Conversa",
+    )
+    fig_avaliacao_conversa.update_layout(
+        plot_bgcolor="rgba(255, 255, 255, 0.9)",
+        paper_bgcolor="rgba(255, 255, 255, 0.9)",
+    )
+
+    feedback_counts = filtered_df["feedback"].value_counts().reset_index()
+    feedback_counts.columns = ["feedback", "count"]
+
+    fig_feedback = px.bar(
+        feedback_counts,
+        x="feedback",
+        y="count",
+        title="Distribuição de Feedback",
+    )
+    fig_feedback.update_layout(
+        plot_bgcolor="rgba(255, 255, 255, 0.9)",
+        paper_bgcolor="rgba(255, 255, 255, 0.9)",
+    )
+
+    return (
+        fig_respostas,
+        fig_mapa,
+        fig_media_conversas,
+        fig_total_diario,
+        fig_tempo_medio_chat,
+        fig_total_interacoes,
+        fig_avaliacao_conversa,
+        fig_feedback,
+    )
 if __name__ == "__main__":
     app.run_server(debug=True, use_reloader=True)
